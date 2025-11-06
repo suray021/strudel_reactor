bde506554c4e5fa930c26bb8a69bd8befb3fe0d0
Ashlesha Surjuse <115522438+suray021@users.noreply.github.com>
2025-11-06 22:10:38 +1030

    Move Strudel demo into routes/Dashboard.jsx

---
+++ src/Dashboard.jsx
@@ -0,0 +1,136 @@
+import './App.css';
+import { useEffect, useRef } from "react";
+import { StrudelMirror } from '@strudel/codemirror';
+import { evalScope } from '@strudel/core';
+import { drawPianoroll } from '@strudel/draw';
+import { initAudioOnFirstClick } from '@strudel/webaudio';
+import { transpiler } from '@strudel/transpiler';
+import { getAudioContext, webaudioOutput, registerSynthSounds } from '@strudel/webaudio';
+import { registerSoundfonts } from '@strudel/soundfonts';
+import { stranger_tune } from '../tunes';
+import console_monkey_patch, { getD3Data } from '../console-monkey-patch';
+
+let globalEditor = null;
+
+const handleD3Data = (event) => {
+    console.log(event.detail);
+};
+
+export function SetupButtons() {
+    document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
+    document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
+    document.getElementById('process').addEventListener('click', () => { Proc(); });
+    document.getElementById('process_play').addEventListener('click', () => {
+        if (globalEditor != null) {
+            Proc();
+            globalEditor.evaluate();
+        }
+    });
+}
+
+export function ProcAndPlay() {
+    if (globalEditor != null && globalEditor.repl.state.started === true) {
+        Proc();
+        globalEditor.evaluate();
+    }
+}
+
+export function Proc() {
+    const proc_text = document.getElementById('proc').value;
+    const proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
+    ProcessText(proc_text);
+    globalEditor.setCode(proc_text_replaced);
+}
+
+export function ProcessText() {
+    let replace = "";
+    if (document.getElementById('flexRadioDefault2').checked) {
+        replace = "_";
+    }
+    return replace;
+}
+
+export default function Dashboard() {
+    const hasRun = useRef(false);
+
+    useEffect(() => {
+        if (!hasRun.current) {
+            document.addEventListener("d3Data", handleD3Data);
+            console_monkey_patch();
+            hasRun.current = true;
+
+            // init canvas
+            const canvas = document.getElementById('roll');
+            canvas.width = canvas.width * 2;
+            canvas.height = canvas.height * 2;
+            const drawContext = canvas.getContext('2d');
+            const drawTime = [-2, 2]; // time window of drawn haps
+
+            globalEditor = new StrudelMirror({
+                defaultOutput: webaudioOutput,
+                getTime: () => getAudioContext().currentTime,
+                transpiler,
+                root: document.getElementById('editor'),
+                drawTime,
+                onDraw: (haps, time) => drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
+                prebake: async () => {
+                    initAudioOnFirstClick(); // don’t await
+                    const loadModules = evalScope(
+                        import('@strudel/core'),
+                        import('@strudel/draw'),
+                        import('@strudel/mini'),
+                        import('@strudel/tonal'),
+                        import('@strudel/webaudio'),
+                    );
+                    await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
+                },
+            });
+
+            document.getElementById('proc').value = stranger_tune;
+            SetupButtons();
+            Proc();
+        }
+    }, []);
+
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <main>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                            <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
+                            <textarea className="form-control" rows="15" id="proc" />
+                        </div>
+                        <div className="col-md-4">
+                            <nav className="d-flex gap-2 flex-wrap">
+                                <button id="process" className="btn btn-outline-primary">Preprocess</button>
+                                <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
+                                <button id="play" className="btn btn-outline-primary">Play</button>
+                                <button id="stop" className="btn btn-outline-primary">Stop</button>
+                            </nav>
+                        </div>
+                    </div>
+
+                    <div className="row mt-3">
+                        <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                            <div id="editor" />
+                            <div id="output" />
+                        </div>
+                        <div className="col-md-4">
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
+                                <label className="form-check-label" htmlFor="flexRadioDefault1">p1: ON</label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
+                                <label className="form-check-label" htmlFor="flexRadioDefault2">p1: HUSH</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <canvas id="roll"></canvas>
+            </main>
+        </div>
+    );
+}
